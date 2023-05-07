b98c5991c11d0d9d6b1fe49c2ab4877daedd5fc5
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-08 00:18:00 +0900

    test

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const data = {
-    id: "0001",
-    name: "AAA",
-    age: 20,
-  };
-  // ここから修正
-  const jsonData = JSON.stringify(data, null, " ");
-  console.log(jsonData);
+function base_status() 
+{
+  const char_name = document.getElementById("char_name").value;
+  fetch("./"+char_name)
+.then(response => response.json())
+.then(data => {
+const hp = data.ステータス.基礎HP["90"];
+document.getElementById("hp").textContent = hp*10000;
+})
+.catch(error => console.error(error));
+}
