aa41bd5f0ad0ce7cabe7eb493e6949a2c6dd2f30
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-09 21:40:31 +0900

    test

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showTable() {
   document.getElementById("myTable").style.display = "table";
-  base_status();
+  calculate_base_status();
 }
 
 //////////////////
@@ -15,8 +15,8 @@ function base_status()
     {
     const base_hp = data.ステータス.基礎HP["90"];
     document.getElementById("base_hp").textContent = base_hp;
-    const base_attck = data.ステータス.基礎攻撃力["90"];
-    document.getElementById("base_attck").textContent = base_attck;
+    const bas_attck = data.ステータス.基礎攻撃力["90"];
+    document.getElementById("bas_attck").textContent = bas_attck;
     const base_elm = data.ステータス.元素熟知["90"];
     document.getElementById("base_elm").textContent = base_elm;
   })
@@ -156,12 +156,84 @@ function calculateDamage ()
   result = "ループ回数: " + n.toFixed(2) +"  聖遺物スコア: " + score.toFixed(1) +"  攻撃力: " + f_attck.toFixed(0)+"  元素熟知: " + f_elm.toFixed(0) +"  会心率: " + f_cr.toFixed(3) + "  会心ダメージ：" + f_cd.toFixed(3) + "激化ダメージ期待値: "+f_agg_exp_dmg.toFixed(0) + "ダメージバフ: "+f_dmg_buff.toFixed(3);
   document.getElementById("result").innerHTML = result;
 }
-fetch('./data/character/nahida.json')
-.then(response => response.json())
-.then(data => {
-　// Display the data
-document.getElementById('data').textContent = JSON.stringify(data, null, 2);
-  });
+
+/////////
 
 
-/////////////////////
+function calculate_char_base_status()
+ {
+  const char_name = document.getElementById("char_name").value;
+  fetch("./data/character/" + char_name + ".json")
+    .then(response => response.json())
+    .then(data => {
+       const char_base_hp = data.ステータス.基礎HP["90"];
+       document.getElementById("char_base_hp").textContent = char_base_hp;
+       const char_base_attck = data.ステータス.基礎攻撃力["90"];
+       document.getElementById("char_base_attck").textContent = char_base_attck;
+       const char_base_deff = data.ステータス.基礎防御力["90"];
+       document.getElementById("char_base_deff").textContent = char_base_deff;
+       const char_base_elm = data.ステータス.基礎元素熟知["90"];
+       document.getElementById("char_base_elm").textContent = char_base_elm;
+       const char_base_elm_charge = data.ステータス.基礎元素チャージ効率["90"];
+       document.getElementById("char_base_elm_charge").textContent = char_base_elm_charge;
+       const char_base_cr = data.ステータス.基礎会心率["90"];
+       document.getElementById("char_base_cr").textContent = char_base_cr;
+       const char_base_cd = data.ステータス.基礎会心ダメージ["90"];
+       document.getElementById("char_base_cd").textContent = char_base_cd;
+       const char_base_dmg_buff = data.ステータス.基礎ダメージバフ["90"];
+       document.getElementById("char_base_dmg_buff").textContent = char_base_dmg_buff;
+        
+       const char_base_status = [char_base_hp, char_base_attck, char_base_deff, char_base_elm, char_base_elm_charge, char_base_cr, char_base_cd, char_base_dmg_buff];
+       console.log(char_base_status);
+      })
+      .catch(error => console.error(error));
+  }
+
+///////////////  
+
+
+function calculate_weapon_base_status()
+ {
+  const weapon_name = document.getElementById("weapon_name").value;
+  fetch("./data/weapon/" + weapon_name + ".json")
+    .then(response => response.json())
+    .then(data => {
+       const weapon_base_hp = data.ステータス.基礎HP["90"];
+       document.getElementById("weapon_base_hp").textContent = weapon_base_hp;
+       const weapon_base_attck = data.ステータス.基礎攻撃力["90"];
+       document.getElementById("weapon_base_attck").textContent = weapon_base_attck;
+       const weapon_base_deff = data.ステータス.基礎防御力["90"];
+       document.getElementById("weapon_base_deff").textContent = weapon_base_deff;
+       const weapon_base_elm = data.ステータス.基礎元素熟知["90"];
+       document.getElementById("weapon_base_elm").textContent = weapon_base_elm;
+       const weapon_base_elm_charge = data.ステータス.基礎元素チャージ効率["90"];
+       document.getElementById("weapon_base_elm_charge").textContent = weapon_base_elm_charge;
+       const weapon_base_cr = data.ステータス.基礎会心率["90"];
+       document.getElementById("weapon_base_cr").textContent = weapon_base_cr;
+       const weapon_base_cd = data.ステータス.基礎会心ダメージ["90"];
+       document.getElementById("weapon_base_cd").textContent = weapon_base_cd;
+       const weapon_base_dmg_buff = data.ステータス.基礎ダメージバフ["90"];
+       document.getElementById("weapon_base_dmg_buff").textContent = weapon_base_dmg_buff;
+       const weapon_base_status = [weapon_base_hp, weapon_base_attck, weapon_base_deff, weapon_base_elm, weapon_base_elm_charge, weapon_base_cr, weapon_base_cd, weapon_base_dmg_buff];
+       console.log(weapon_base_status);
+      })
+      .catch(error => console.error(error));
+  }
+////////
+
+
+function calculate_base_status()
+ {
+  calculate_char_base_status()
+  calculate_weapon_base_status()
+  const base_hp = char_base_status[0] + weapon_base_status[0];
+  const base_attck = char_base_status[1] + weapon_base_status[1];
+  const base_deff = char_base_status[2] + weapon_base_status[2];
+  const base_elm = char_base_status[3] + weapon_base_status[3];
+  const base_elm_charge = char_base_status[4] + weapon_base_status[4];
+  const base_cr = char_base_status[5] + weapon_base_status[5];
+  const base_cd = char_base_status[6] + weapon_base_status[6];
+  const base_dmg_buff = char_base_status[7] + weapon_base_status[7];
+  const base_status = [base_hp, base_attck, base_deff, base_elm, base_elm_charge, base_cr, base_cd, base_dmg_buff];
+  console.log(weapon_base_status);  
+  }
