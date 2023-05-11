dc2765d952374934023152456aea6ec4a8697e2c
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-12 04:01:56 +0900

    test

--- script.js
+++ script.js
@@ -100,8 +100,8 @@ async function calculate_af_score() {
 
   async function showForm()
    {
-    const char_name = document.getElementById("char_name").value;
-    const response = await fetch("./data/character/" + char_name + ".json");
+    const charname = document.getElementById("charname").value;
+    const response = await fetch("./data/character/" + charname + ".json");
     const data = await response.json();
     const depend_status = data.ステータス.依存ステータス;
     // フォームを表示するために、フォームのコンテナーを表示に変更します。
