197f57051d65d36c83dc6f82527003c460145dde
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-16 17:52:42 +0900

    test

--- script.js
+++ script.js
@@ -519,7 +519,7 @@ async function monte_carlo_calculate()
   let score_distribute = await calculate_score_distribute(af_score,depend_status);
   
   let fixed_status = [0,0,0,0,0,0,0,0];
-  let result_status = [0,0,0,0,0,0,0,0];
+  let result_status = [0,0,0,100000,0,0,0,0];
   const char_instance = await create_char_instance(base_status, fixed_status, result_status);
   const weapon_instance = await create_weapon_instance(base_status, fixed_status, result_status);
 
