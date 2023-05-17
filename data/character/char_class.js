4b4f2088570b084585e76b8def97665b69364298
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-17 13:47:18 +0900

    test

---
+++ data/character/char_class.js
@@ -0,0 +1,77 @@
+class nahida {
+    constructor(base_status_array, fixed_status_array, result_status_array) 
+    {
+      this.base_status_array = base_status_array;
+      this.fixed_status_array = fixed_status_array;
+      this.result_status_array = result_status_array;
+    }
+  
+    calculate_char_fixed_hp() {
+      return  0;
+    }
+  
+    calculate_char_result_hp() {
+      return 0;
+    }
+  
+    calculate_char_fixed_attck() {
+      return 0;
+    }
+  
+    calculate_char_result_attck() {
+      return 0;
+    }
+  
+    calculate_char_fixed_deff() {
+      return 0;
+    }
+  
+    calculate_char_result_deff() {
+      return 0;
+    }
+  
+    calculate_char_fixed_elm() {
+      return 0;
+    }
+  
+    calculate_char_result_elm() {
+      return 0;
+    }
+  
+    calculate_char_fixed_elm_charge() {
+      return 0;
+    }
+  
+    calculate_char_result_elm_charge() {
+      return 0;
+    }
+  
+    calculate_char_fixed_cr() {
+      return  0;
+    }
+  
+    calculate_char_result_cr() {
+      return Math.min(Math.max(0,(this.result_status_array[3]-200)),800)*0.0003;
+    }
+  
+    calculate_char_fixed_cd() {
+      return 0;
+    }
+  
+    calculate_char_result_cd() {
+      return 0;
+    }
+  
+    calculate_char_fixed_dmg_buff() {
+      return 0.15;
+    }
+  
+    calculate_char_result_dmg_buff() {
+      return 0.15 + Math.min(Math.max(0,(this.result_status_array[3]-200)),800)*0.001;
+    }
+  
+    update_status(fixed_status_array, result_status_array) {
+      this.fixed_status_array = fixed_status_array;
+      this.result_status_array = result_status_array;
+    }
+  }
---
+++ data/weapon/weapon_class.js
@@ -0,0 +1,79 @@
+class AThousandFloatingDreams {
+    constructor(base_status_array, fixed_status_array, result_status_array) 
+    {
+      this.base_status_array = base_status_array;
+      this.fixed_status_array = fixed_status_array;
+      this.result_status_array = result_status_array;
+    }
+  
+    calculate_weapon_fixed_hp() {
+      return 0;
+    }
+  
+    calculate_weapon_result_hp() {
+      return 0;
+    }
+  
+    calculate_weapon_fixed_attck() {
+      return 0;
+    }
+  
+    calculate_weapon_result_attck() {
+      return 0;
+    }
+  
+    calculate_weapon_fixed_deff() {
+      return 0;
+    }
+  
+    calculate_weapon_result_deff() {
+      return this.result_status_array[2] = 0;
+    }
+  
+    calculate_weapon_fixed_elm() {
+      return 32;
+    }
+  
+    calculate_weapon_result_elm() {
+  
+      return 32;
+    }
+  
+    calculate_weapon_fixed_elm_charge() {
+      return 0;
+    }
+  
+    calculate_weapon_result_elm_charge() {
+      return 0;
+    }
+  
+    calculate_weapon_fixed_cr() {
+      return 0;
+    }
+  
+    calculate_weapon_result_cr() {
+      return 0;
+    }
+  
+    calculate_weapon_fixed_cd() {
+      return 0;
+    }
+  
+    calculate_weapon_result_cd() {
+      return 0;
+    }
+  
+    calculate_weapon_fixed_dmg_buff() {
+      return 0.2;
+    }
+  
+    calculate_weapon_result_dmg_buff() {
+      return 0.2;
+    }
+  
+    update_status(fixed_status_array, result_status_array) {
+      this.fixed_status_array = fixed_status_array;
+      this.result_status_array = result_status_array;
+    }
+  
+  }
