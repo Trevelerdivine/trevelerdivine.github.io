ac708b1546eee01fe805bbc137990199d1b60bfd
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-18 22:48:12 +0900

    fix

--- data/character/char_class.js
+++ data/character/char_class.js
@@ -10,7 +10,7 @@ class nahida {
       const response = await fetch("./data/character/nahida.json");
       const data = await response.json();
       const dmg_attck_rate = data.元素スキル.数値.攻撃力[10];
-      const dmg_elm_rate = data.元素スキル.数値.攻撃力[10];
+      const dmg_elm_rate = data.元素スキル.数値.元素熟知[10];
       const dmg_rate = [0,dmg_attck_rate,0,dmg_elm_rate,0,0]; 
       return dmg_rate
     }
