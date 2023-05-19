ad1805f915cd6c7bfae7b0cc860134a17ddbbc3d
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-19 19:31:54 +0900

    test

--- data/weapon/weapon_class.js
+++ data/weapon/weapon_class.js
@@ -183,7 +183,7 @@ class AThousandFloatingDreams {
     }
   
     calculate_weapon_result_attck() {
-      return Math.min(this.result_status_array[4]/100- 1, 0.8/0.28)*0.28*this.base_status_array[1];
+      return Math.min(this.result_status_array[4]- 1, 0.8/0.28)*0.28*this.base_status_array[1];
     }
   
     calculate_weapon_fixed_deff() {
