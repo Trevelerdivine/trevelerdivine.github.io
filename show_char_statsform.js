1063d2825be08954f4ab88f440d02f3e16a9293d
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-21 23:35:17 +0900

    test

--- show_char_statsform.js
+++ show_char_statsform.js
@@ -1,5 +1,6 @@
 async function show_char_statsform()
    {
+
     await calculate_depend_status();
     const lineBreak = document.createElement("br");
     let hp_form = document.getElementById("hp_form");
@@ -50,7 +51,6 @@ async function show_char_statsform()
     {
       cd_form.style.display = "block";  // 会心ダメージフォームを表示
     }
-
     const characterSelect = document.getElementById("char_index");
     const selectedCharacter = characterSelect.value;
     const characterInfo = document.getElementById("characterInfo");
@@ -68,10 +68,34 @@ async function show_char_statsform()
 
       const traitLabel = document.createElement("label");
       traitLabel.htmlFor = "traitCheckbox";
-      traitLabel.textContent = "心景幻成: 元素熟知+250";
+      traitLabel.textContent = "1重：心識蘊蔵の種";
+
+      const traitCheckbox2 = document.createElement("input");
+      traitCheckbox2.type = "checkbox";
+      traitCheckbox2.id = "traitCheckbox2";
+      traitCheckbox2.value = "traitCheckbox2";
+
+      const traitLabel2 = document.createElement("label");
+      traitLabel2.htmlFor = "traitCheckbox2";
+      traitLabel2.textContent = "2重：正覚善見の根 激化で防御力-20%";
+
+      const traitCheckbox3 = document.createElement("input");
+      traitCheckbox3.type = "checkbox";
+      traitCheckbox3.id = "traitCheckbox3";
+      traitCheckbox3.value = "traitCheckbox3";
+
+      const traitLabel3 = document.createElement("label");
+      traitLabel3.htmlFor = "traitCheckbox3";
+      traitLabel3.textContent = "4重：正覚善見の根 激化で防御力-20%";
 
       characterInfo.appendChild(traitCheckbox);
       characterInfo.appendChild(traitLabel);
+      characterInfo.appendChild(lineBreak);
+      characterInfo.appendChild(traitCheckbox2);
+      characterInfo.appendChild(traitLabel2);
+      characterInfo.appendChild(lineBreak);
+      characterInfo.appendChild(traitCheckbox3);
+      characterInfo.appendChild(traitLabel3);
     } else if (selectedCharacter === "3") {
       // 雷電将軍の特性を考慮するかを選ぶチェックボックスを表示
       const traitCheckbox = document.createElement("input");
