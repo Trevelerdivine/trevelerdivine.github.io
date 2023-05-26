1181d2bdc07d03b34f08262c2d26f240ebc6f0f8
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-05-26 22:48:44 +0900

    fix

--- script.js
+++ script.js
@@ -383,7 +383,7 @@ const levelObject = levelData["レベル"];
 const aggobject = levelData["反応固有値"]; 
 const selectedLevel = levelObject[level_index];
 const agg_fixed_value = aggobject[selectedLevel];
-const char_constellations = document.getElementById(char_constellations);
+const char_constellations = document.getElementById("char_constellations");
 const constellations = char_constellations.value;
 const parameter = [selectedLevel,agg_fixed_value,constellations];
 console.log(parameter);
