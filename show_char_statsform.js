2b4311fa88a778f45e10aa63b8898a9946b5a685
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-09-15 13:04:23 +0900

    test

--- show_char_statsform.js
+++ show_char_statsform.js
@@ -1,88 +1,143 @@
-async function show_char_statsform() {
-  await calculate_depend_status();
-  const char_constellations = document.getElementById("char_constellations").value;
+async function show_char_statsform()
+   {
+    await calculate_depend_status();
+    let hp_form = document.getElementById("hp_form");
+    let attck_form = document.getElementById("attck_form");
+    let deff_form = document.getElementById("deff_form");
+    let elm_form = document.getElementById("elm_form");
+    let elm_charge_form = document.getElementById("elm_charge_form");
+    let cr_form = document.getElementById("cr_form");
+    let cd_form = document.getElementById("cd_form");
+    let team_hp_form = document.getElementById("team_hp_form");
+    let team_hprate_form = document.getElementById("team_hprate_form");
+    let team_attack_form = document.getElementById("team_attack_form");
+    let team_attackrate_form = document.getElementById("team_attackrate_form");
+    let team_deff_form = document.getElementById("team_deff_form");
+    let team_deffrate_form = document.getElementById("team_deffrate_form");
+    let team_elm_form = document.getElementById("team_elm_form");
+    let team_elm_charge_form = document.getElementById("team_elm_charge_form");
+    let team_cr_form = document.getElementById("team_cr_form");
+    let team_cd_form = document.getElementById("team_cd_form");
+    const char_constellations = document.getElementById("char_constellations").value;
 
-  // フォーム要素のIDをまとめる
-  const formIds = [
-    "hp_form", "attck_form", "deff_form", "elm_form", "elm_charge_form",
-    "cr_form", "cd_form", "team_hp_form", "team_hprate_form",
-    "team_attack_form", "team_attackrate_form", "team_deff_form",
-    "team_deffrate_form", "team_elm_form", "team_elm_charge_form",
-    "team_cr_form", "team_cd_form"
-  ];
+    hp_form.style.display = "none";  // HPフォームを非表示
+    attck_form.style.display = "none";  // 攻撃力フォームを非表示
+    deff_form.style.display = "none";  // 防御力フォームを非表示
+    elm_form.style.display = "none";  // 元素熟知を非表示
+    elm_charge_form.style.display = "none";  // 元素チャージ効率フォームを非表示
+    cr_form.style.display = "none";  // 会心率フォームを非表示
+    cd_form.style.display = "none";  // 会心ダメージフォームを非表示
+    team_hp_form.style.display = "none";
+    team_hprate_form.style.display = "none";
+    team_attack_form.style.display = "none";
+    team_attackrate_form.style.display = "none";
+    team_deff_form.style.display = "none";
+    team_deffrate_form.style.display = "none";
+    team_elm_form.style.display = "none";
+    team_elm_charge_form.style.display = "none";
+    team_cr_form.style.display = "none";
+    team_cd_form.style.display = "none";
+    
+    const characterInfo = document.getElementById("characterInfo");
+    const checkboxStates = []; // チェックボックスの状態を格納する配列
+    characterInfo.style.display = "block";
 
-  // フォーム要素を非表示にする
-  formIds.forEach(id => {
-    const form = document.getElementById(id);
-    if (form) {
-      form.style.display = "none";
-    }
-  });
-
-  const characterInfo = document.getElementById("characterInfo");
-  const checkboxStates = []; // チェックボックスの状態を格納する配列
-  characterInfo.style.display = "block";
-  characterInfo.innerHTML = "";
-
-  if (selectedCharId === "56" || selectedCharId === "34") {
-    const traits = [
-      {
-        id: "traitCheckbox",
-        label: "第1重：心識蘊蔵の種"
-      },
-      {
-        id: "traitCheckbox2",
-        label: "第2重：正覚善見の根 激化で防御力-30%"
-      },
-      {
-        id: "traitCheckbox3",
-        label: "第4重：比量現行の茎 敵の人数に応じて熟知バフ"
-      },
-      {
-        id: "traitCheckbox4",
-        label: "第6重：大辯円成の実 追撃"
-      }
-    ];
-
-    if (char_constellations > 0) {
-      for (let i = 0; i < char_constellations; i++) {
-        const traitCheckbox = document.createElement("input");
-        traitCheckbox.type = "checkbox";
-        traitCheckbox.id = traits[i].id;
-        traitCheckbox.value = traits[i].id;
-        traitCheckbox.checked = true;
-
-        const traitLabel = document.createElement("label");
-        traitLabel.htmlFor = traits[i].id;
-        traitLabel.textContent = traits[i].label;
-
-        characterInfo.appendChild(traitCheckbox);
-        characterInfo.appendChild(traitLabel);
-
-        const lineBreak = document.createElement("br");
-        characterInfo.appendChild(lineBreak);
-      }
-    }
-  }
-
-  const formElements = [
-    { forms: ["hp_form", "team_hp_form", "team_hprate_form"], index: 0 },
-    { forms: ["attck_form", "team_attack_form", "team_attackrate_form"], index: 4 },
-    { forms: ["deff_form", "team_deff_form", "team_deffrate_form"], index: 1 },
-    { forms: ["elm_form", "team_elm_form"], index: 2 },
-    { forms: ["elm_charge_form", "team_elm_charge_form"], index: 3 },
-    { forms: ["cr_form", "team_cr_form"], index: 5 },
-    { forms: ["cd_form", "team_cd_form"], index: 6 }
-  ];
-
-  for (const element of formElements) {
-    if (depend_status[element.index] === 1) {
-      element.forms.forEach(formId => {
-        const form = document.getElementById(formId);
-        if (form) {
-          form.style.display = "table-row";
+    characterInfo.innerHTML = "";
+    
+    if (selectedCharId  === "56") {
+      const traits = [
+        {
+          id: "traitCheckbox",
+          label: "第1重：心識蘊蔵の種"
+        },
+        {
+          id: "traitCheckbox2",
+          label: "第2重：正覚善見の根 激化で防御力-30%"
+        },
+        {
+          id: "traitCheckbox3",
+          label: "第4重：比量現行の茎 敵の人数に応じて熟知バフ"
+        },
+        {
+          id: "traitCheckbox4",
+          label: "第6重：大辯円成の実 追撃"
         }
-      });
+      ];
+      if (char_constellations > 0)
+      {
+        for (let i = 0; i < char_constellations; i++) {
+          const traitCheckbox = document.createElement("input");
+          traitCheckbox.type = "checkbox";
+          traitCheckbox.id = traits[i].id;
+          traitCheckbox.value = traits[i].id;
+          traitCheckbox.checked = true;
+        
+          const traitLabel = document.createElement("label");
+          traitLabel.htmlFor = traits[i].id;
+          traitLabel.textContent = traits[i].label;
+        
+          characterInfo.appendChild(traitCheckbox);
+          characterInfo.appendChild(traitLabel);
+          const lineBreak = document.createElement("br");
+          characterInfo.appendChild(lineBreak);
+        }
+      }
+    } else if (selectedCharId  === "34")
+      {
+        const traits = [
+          {
+            id: "traitCheckbox",
+            label: "第1重：悪曜の呪詛 眼力溜まりやすさup"
+          },
+          {
+            id: "traitCheckbox2",
+            label: "第2重：斬鉄断金 防御力60%無視"
+          },
+          {
+            id: "traitCheckbox3",
+            label: "第4重：常道への誓い 雷電将軍以外の攻撃力+30%"
+          },
+          {
+            id: "traitCheckbox4",
+            label: "第6重：願いの代行者 雷電将軍以外の元素爆発クールタイム減少"
+          }
+        ];
+        if (char_constellations > 0)
+        {
+          for (let i = 0; i < char_constellations; i++) 
+          {
+            const traitCheckbox = document.createElement("input");
+            traitCheckbox.type = "checkbox";
+            traitCheckbox.id = traits[i].id;
+            traitCheckbox.value = traits[i].id;
+            traitCheckbox.checked = true;
+          
+            const traitLabel = document.createElement("label");
+            traitLabel.htmlFor = traits[i].id;
+            traitLabel.textContent = traits[i].label;
+          
+            characterInfo.appendChild(traitCheckbox);
+            characterInfo.appendChild(traitLabel);
+            const lineBreak = document.createElement("br");
+            characterInfo.appendChild(lineBreak);
+          }
+        }
+      }
+      const formElements = [
+        { forms: [hp_form, team_hp_form, team_hprate_form], index: 0 },
+        { forms: [attck_form, team_attack_form, team_attackrate_form], index: 4 },
+        { forms: [deff_form, team_deff_form, team_deffrate_form], index: 1 },
+        { forms: [elm_form, team_elm_form], index: 2 },
+        { forms: [elm_charge_form, team_elm_charge_form], index: 3 },
+        { forms: [cr_form, team_cr_form], index: 5 },
+        { forms: [cd_form, team_cd_form], index: 6 }
+      ];
+      
+      for (const element of formElements) {
+        if (depend_status[element.index] === 1) {
+          for (let i = 0; i < element.forms.length; i++) {
+            element.forms[i].style.display = "table-row";
+          }
+        }
+      }
     }
-  }
-}
