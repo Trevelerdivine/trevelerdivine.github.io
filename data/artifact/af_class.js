cfd6cc5577a0823f442b4b9b800389fa8e114518
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-09-17 04:22:08 +0900

    test

--- data/artifact/af_class.js
+++ data/artifact/af_class.js
@@ -56,12 +56,8 @@ class af1_4
 
 
 
-const set_effect2 = [af0_2, af1_2, af2_2, af3_2, af4_2, af5_2, af6_2, af7_2, af8_2, af9_2, af10_2,
-    af11_2, af12_2, af13_2, af14_2, af15_2, af16_2, af17_2, af18_2, af19_2, af20_2,
-    af21_2, af22_2, af23_2, af24_2, af25_2
+const set_effect2 = [af0_2, af1_2
    ];
   
-  const set_effect4 = [ af0_4, af1_4, af2_4, af3_4, af4_4, af5_4, af6_4, af7_4, af8_4, af9_4, af10_4,
-     af11_4, af12_4, af13_4, af14_4, af15_4, af16_4, af17_4, af18_4, af19_4, af20_4,
-     af21_4, af22_4, af23_4, af24_4, af25_4
+  const set_effect4 = [ af0_4, af1_4
    ];
