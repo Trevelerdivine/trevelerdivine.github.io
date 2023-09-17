77caa5a15d054099c7ff9333d66792dc4efb4b42
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-09-17 13:59:38 +0900

    test

--- data/artifact/af_class.js
+++ data/artifact/af_class.js
@@ -1,63 +1,583 @@
-class af0_2
+
+function af0_2()
 {
-   set_buff()
-   {
-        let af0_2_checkbox = document.getElementById("af0_2");
-        let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
-        if (af0_2_checkbox.checked)
-        {
-            status_buff = [0, 0, 0, 0, 0.18, 0, 0, 0]; 
-        }
-        return status_buff
-   }
+    let checkbox = document.getElementById("af0_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0.18, 0, 0, 0]; 
+    }
+    return status_buff
 }
 
-class af0_4
+function af0_4()
 {
-   set_buff()
-   {
-        let af0_4_checkbox = document.getElementById("af0_4");
-        let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
-        if (af0_4_checkbox.checked)
-        {
-            status_buff = [0, 0, 0, 0, 0, 0, 0, 0.35]; 
-        }
-        return status_buff
-   }
+    let checkbox = document.getElementById("af0_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.35]; 
+    }
+    return status_buff
 }
 
-class af1_2
+function af1_2()
 {
-   set_buff()
-   {
-        let af1_2_checkbox = document.getElementById("af1_2");
-        let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
-        if (af1_2_checkbox.checked)
-        {
-            status_buff = [0, 0, 80, 0, 0, 0, 0, 0]; 
-        }
-        return status_buff
-   }
+    let checkbox = document.getElementById("af1_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 80, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
 }
 
-class af1_4
+function af1_4()
 {
-   set_buff()
-   {
-        let af1_4_checkbox = document.getElementById("af1_4");
-        let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
-        if (af1_4_checkbox.checked)
-        {
-            status_buff = [0, 0, 0, 0, 0, 0, 0, 0.35]; 
-        }
-        return status_buff
-   }
+    let checkbox = document.getElementById("af1_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.35]; 
+    }
+    return status_buff
+}
+
+function af2_2()
+{
+    let checkbox = document.getElementById("af2_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.2]; 
+    }
+    return status_buff
+}
+
+function af2_4()
+{
+    let checkbox = document.getElementById("af2_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0.2, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af3_2()
+{
+    let checkbox = document.getElementById("af3_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.2]; 
+    }
+    return status_buff
+}
+
+function af3_4()
+{
+    let checkbox = document.getElementById("af3_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.5]; 
+    }
+    return status_buff
+}
+
+function af4_2()
+{
+    let checkbox = document.getElementById("af4_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af4_4()
+{
+    let checkbox = document.getElementById("af4_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af5_2()
+{
+    let checkbox = document.getElementById("af5_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af5_4()
+{
+    let checkbox = document.getElementById("af5_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.4]; 
+    }
+    return status_buff
+}
+
+function af6_2()
+{
+    let checkbox = document.getElementById("af6_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af6_4()
+{
+    let checkbox = document.getElementById("af6_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.35]; 
+    }
+    return status_buff
+}
+
+function af7_2()
+{
+    let checkbox = document.getElementById("af7_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af7_4()
+{
+    let checkbox = document.getElementById("af7_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.4]; 
+    }
+    return status_buff
+}
+
+function af8_2()
+{
+    let checkbox = document.getElementById("af8_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af8_4()
+{
+    let checkbox = document.getElementById("af8_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.35]; 
+    }
+    return status_buff
+}
+
+function af9_2()
+{
+    let checkbox = document.getElementById("af9_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af9_4()
+{
+    let checkbox = document.getElementById("af9_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.2]; 
+    }
+    return status_buff
+}
+
+function af10_2()
+{
+    let checkbox = document.getElementById("af10_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af10_4()
+{
+    let checkbox = document.getElementById("af10_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.35]; 
+    }
+    return status_buff
+}
+
+function af11_2()
+{
+    let checkbox = document.getElementById("af11_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af11_4()
+{
+    let checkbox = document.getElementById("af11_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.225]; 
+    }
+    return status_buff
+}
+
+function af12_2()
+{
+    let checkbox = document.getElementById("af12_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af12_4()
+{
+    let checkbox = document.getElementById("af12_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0.4, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af13_2()
+{
+    let checkbox = document.getElementById("af13_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af13_4()
+{
+    let checkbox = document.getElementById("af13_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.3]; 
+    }
+    return status_buff
+}
+
+function af14_2()
+{
+    let checkbox = document.getElementById("af14_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0.2, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af14_4()
+{
+    let checkbox = document.getElementById("af14_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0.2, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af15_2()
+{
+    let checkbox = document.getElementById("af15_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.25]; 
+    }
+    return status_buff
+}
+
+function af15_4()
+{
+    let checkbox = document.getElementById("af15_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0.18, 0, 0, 0.25]; 
+    }
+    return status_buff
+}
+
+function af16_2()
+{
+    let checkbox = document.getElementById("af16_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0.18, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af16_4()
+{
+    let checkbox = document.getElementById("af16_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.5]; 
+    }
+    return status_buff
+}
+
+function af17_2()
+{
+    let checkbox = document.getElementById("af17_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af17_4()
+{
+    let checkbox = document.getElementById("af17_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af18_2()
+{
+    let checkbox = document.getElementById("af18_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0.3, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af18_4()
+{
+    let checkbox = document.getElementById("af18_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0.24, 0, 0, 0, 0, 0, 0.24]; 
+    }
+    return status_buff
+}
+
+function af19_2()
+{
+    let checkbox = document.getElementById("af19_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0.18, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af19_4()
+{
+    let checkbox = document.getElementById("af19_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af20_2()
+{
+    let checkbox = document.getElementById("af20_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 80, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af20_4()
+{
+    let checkbox = document.getElementById("a20_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 150, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af21_2()
+{
+    let checkbox = document.getElementById("af21_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af21_4()
+{
+    let checkbox = document.getElementById("af21_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af22_2()
+{
+    let checkbox = document.getElementById("af22_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af22_4()
+{
+    let checkbox = document.getElementById("af22_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.4]; 
+    }
+    return status_buff
+}
+
+function af23_2()
+{
+    let checkbox = document.getElementById("af23_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 80, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af23_4()
+{
+    let checkbox = document.getElementById("af23_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af24_2()
+{
+    let checkbox = document.getElementById("af24_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af24_4()
+{
+    let checkbox = document.getElementById("af24_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0.25, 0, 0, 0.15]; 
+    }
+    return status_buff
+}
+
+function af25_2()
+{
+    let checkbox = document.getElementById("af25_2");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0.2, 0, 0, 0, 0, 0, 0, 0]; 
+    }
+    return status_buff
+}
+
+function af25_4()
+{
+    let checkbox = document.getElementById("af25_4");
+    let status_buff = [0, 0, 0, 0, 0, 0, 0, 0]; 
+    if (checkbox.checked)
+    {
+        status_buff = [0, 0, 0, 0, 0, 0, 0, 0.5]; 
+    }
+    return status_buff
 }
 
 
-
-const set_effect2 = [af0_2, af1_2
+const set_effect2 = [af0_2, af1_2, af2_2, af3_2, af4_2, af5_2, af6_2, af7_2, af8_2, af9_2, af10_2,
+    af11_2, af12_2, af13_2, af14_2, af15_2, af16_2, af17_2, af18_2, af19_2, af20_2,
+    af21_2, af22_2, af23_2, af24_2, af25_2
    ];
   
-  const set_effect4 = [ af0_4, af1_4
+  const set_effect4 = [ af0_4, af1_4, af2_4, af3_4, af4_4, af5_4, af6_4, af7_4, af8_4, af9_4, af10_4,
+     af11_4, af12_4, af13_4, af14_4, af15_4, af16_4, af17_4, af18_4, af19_4, af20_4,
+     af21_4, af22_4, af23_4, af24_4, af25_4
    ];
