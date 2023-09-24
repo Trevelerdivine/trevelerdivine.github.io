e73aa0a64c6a355da3e8efa01828a107dff20759
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-09-24 22:22:27 +0900

    test

--- show_char_statsform.js
+++ show_char_statsform.js
@@ -300,9 +300,6 @@ function showFormElements() {
   let team_elm_charge_form = document.getElementById("team_elm_charge_form");
   let team_cr_form = document.getElementById("team_cr_form");
   let team_cd_form = document.getElementById("team_cd_form");
-
-  calculate_depend_status();
-  console.log(depend_status)
   const formElements = [
     { forms: [hp_form, team_hp_form, team_hprate_form], index: 0 },
     { forms: [attck_form, team_attack_form, team_attackrate_form], index: 4 },
