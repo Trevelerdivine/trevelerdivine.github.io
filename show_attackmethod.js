cf2c935b298c6b7b6b4e9ecee7930dd9af07b711
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-09-24 11:33:41 +0900

    test

---
+++ show_attackmethod.js
@@ -0,0 +1,58 @@
+async function show_attack_method()
+{
+    const select_reaction_method = document.getElementById("select_reaction_method");
+    select_reaction_method.innerHTML = "";
+
+    switch (selectedCharId)
+    {
+        case 56:
+            const select_reaction_method = document.getElementById("select_reaction_method");
+            const attack_index = select_reaction_method.value;
+            
+            let options = [];
+            
+            if (attack_index[0]) {
+              options = [
+                { text: "１段目", value: "0", checked: true },
+                { text: "２段目", value: "1" },
+                { text: "３段目", value: "2" },
+                { text: "４段目", value: "3", checked: true },
+              ];
+            } else if (attack_index[1]) {
+              options = [
+                { text: "重撃", value: "0", checked: true },
+              ];
+            } else if (attack_index[2]) {
+              options = [
+                { text: "滅浄三業", value: "0", checked: true },
+              ];
+            }
+            createRadioButtons(options)
+        break
+    }
+
+}
+
+
+
+function createRadioButtons(options) {
+    // ラジオボタンを生成
+    options.forEach((option) => {
+      const radioLabel = document.createElement("label");
+      radioLabel.textContent = option.text;
+  
+      const radioButton = document.createElement("input");
+      radioButton.type = "radio";
+      radioButton.name = "attack_method"; // 同じ名前を持つラジオボタンは1つの選択肢内で1つのみ選択可能
+      radioButton.value = option.value;
+      if (option.checked) {
+        radioButton.checked = true;
+      }
+  
+      // ラジオボタンとラベルをコンテナに追加
+      const containerElement = document.getElementById("select_reaction_method"); // ここにラジオボタンを追加する要素を指定
+      containerElement.appendChild(radioButton);
+      containerElement.appendChild(radioLabel);
+      containerElement.appendChild(document.createElement("br")); // 改行を追加
+    });
+  }
