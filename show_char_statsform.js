945c62172a690a6ba0f471c0f200a204869faf7b
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-09-26 20:27:20 +0900

    test

--- show_char_statsform.js
+++ show_char_statsform.js
@@ -114,12 +114,17 @@ async function show_char_statsform()
       char_talent.appendChild(otherRadio);
       char_talent.appendChild(createLabel("other-label", "その他"));
       char_talent.appendChild(document.createElement("br"));
-      char_talent.appendChild(createLabel("element-mastery-label", "元素熟知"));
+      char_talent.appendChild(createLabel("element-mastery-label", "　元素熟知"));
 
       // 元素熟知の入力フォームを追加
-      const elementMasteryInput = createInput("text", "element-mastery", "100"); // デフォルト値は "100" ですが、必要に応じて変更してください
+      const elementMasteryInput = createInput("text", "element-mastery", "800"); // デフォルト値は "100" ですが、必要に応じて変更してください
       char_talent.appendChild(elementMasteryInput);
       char_talent.appendChild(document.createElement("br"));
+
+        // 「その他」ラジオボタンのイベントリスナーを追加
+  otherRadio.addEventListener("change", function () {
+    elementMasteryInput.disabled = !otherRadio.checked; // チェックされていない場合はフォームを無効にする
+  });
     
       if (char_constellations > 0) {
         for (let i = 0; i < char_constellations; i++) {
