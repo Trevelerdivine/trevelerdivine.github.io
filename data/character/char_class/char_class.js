f3ee93d065402a00ca9e69ea3eb5a329f25c13e0
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-10-01 02:04:20 +0900

    fix_bug

--- data/character/char_class/char_class.js
+++ data/character/char_class/char_class.js
@@ -260,7 +260,7 @@ class yaemiko {
       dmg_attck_rate = parseFloat(data["元素スキル"]["詳細"][2]["数値"][this.parameter[3]])*3;
       dmg_rate = [0, 0, 0, 0, dmg_attck_rate, 0, 0];
     } else if (attack_method == 21) {
-      const first_dmg_rate = data["元素爆発"]["詳細"]["0"]["数値"][this.parameter[3]];
+      const first_dmg_rate = data["元素爆発"]["詳細"][0]["数値"][this.parameter[3]];
       const second_dmg_rate = data["元素爆発"]["詳細"][1]["数値"][this.parameter[3]];
       const Q_dmg_rate = [first_dmg_rate, second_dmg_rate];
       dmg_rate = [0, 0, 0, 0, Q_dmg_rate, 0, 0];
