6673a5bffd33d07578c016bef99a1eb354d87af8
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-10-08 12:24:10 +0900

    test

--- script.js
+++ script.js
@@ -204,6 +204,7 @@ async function calculate_depend_status()
   const char_data = await char_response.json();
   char_propaty[0] = char_data[attack_method_name[attack_method_index]]["元素"];
   const char_depend_status = char_data[attack_method_name[attack_method_index]].依存ステータス;
+  _status = parseInt(char_data["武器"]);
   const weapon_index = document.getElementById("weapon_index").value;
   const weapon_response = await fetch("./data/weapon/weapon_data/" + weapon_name[weapon_index] + ".json");
   const weapon_data = await weapon_response.json();
