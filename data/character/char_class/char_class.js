7353a6d6365a995a3bb09b6aa183ba9b6f484ddf
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-10-20 11:48:25 +0900

    test

--- data/character/char_class/char_class.js
+++ data/character/char_class/char_class.js
@@ -1641,6 +1641,121 @@ class xiangling {
     }
   }
 
+  class zhongli {
+    constructor(base_status_array, fixed_status_array, result_status_array,parameter) {
+      this.base_status_array = base_status_array;
+      this.fixed_status_array = fixed_status_array;
+      this.result_status_array = result_status_array;
+      this.dmg_rateCache = null;
+      this.parameter = parameter;
+      this.sixth_conste_buff = 0;
+      this.char_constellations = 0;
+      this.weapon_rank = parseInt(document.getElementById("weapon_rank").value);
+    }
+    
+    async dmg_rate_data() {
+      this.char_constellations = document.getElementById("char_constellations").value;
+      
+      // JSON データを取得
+      const response = await fetch("./data/character/char_data/zhongli.json");
+      const data = await response.json();
+    
+      // 攻撃方法に応じてダメージ率を計算
+      let dmg_rate;
+      let dmg_attck_rate = 0;
+      if (attack_method == 21) {
+        dmg_attck_rate += parseFloat(data["元素爆発"]["詳細"][0]["数値"][this.parameter[3]]);
+        dmg_rate = [0, 0, 0, 0, dmg_attck_rate, 0, 0];
+      } 
+      return dmg_rate;
+    }
+    
+    calculate_char_fixed_hp() {
+      return 0;
+    }
+  
+    calculate_char_result_hp() {
+      return 0;
+    }
+  
+    calculate_char_fixed_attck() {
+      return 0;
+    }
+  
+    calculate_char_result_attck() {
+      return 0;
+    }
+  
+    calculate_char_fixed_deff() {
+      return 0;
+    }
+  
+    calculate_char_result_deff() {
+      return 0;
+    }
+  
+    calculate_char_fixed_elm() {
+      return 0;
+    }
+  
+    calculate_char_result_elm() {
+      return 0;
+    }
+  
+    calculate_char_fixed_elm_charge() {
+      return 0;
+    }
+  
+    calculate_char_result_elm_charge() {
+      return 0;
+    }
+  
+    calculate_char_fixed_cr() {
+      return 0;
+    }
+  
+    calculate_char_result_cr() {
+      return 0;
+    }
+  
+    calculate_char_fixed_cd() {
+      return 0;
+    }
+  
+    calculate_char_result_cd() {
+      return 0;
+    }
+  
+    calculate_char_fixed_dmg_buff() {
+      return 0;
+    }
+  
+    calculate_char_result_dmg_buff() {
+      return 0;
+    }
+  
+    calculate_basic_dmg(dmg_rate) {
+      let resultStatusArray = this.result_status_array;
+      let basicDmg;
+      if (attack_method == 21)
+      {
+        basicDmg = dmg_rate[4] * resultStatusArray[4] + resultStatusArray[0] * 0.33;
+        return basicDmg;
+      }
+    }
+  
+    update_status(fixed_status_array, result_status_array)
+    {
+      this.fixed_status_array = fixed_status_array;
+      this.result_status_array = result_status_array;
+    }
+  
+    calculate_char_debuff() {
+      let char_debuff = [0,0,0];
+      return char_debuff;
+    }
+  }
+
 
   function calculate_weapon_basedmg (attack_count, status_array, weapon_rank)
   {
