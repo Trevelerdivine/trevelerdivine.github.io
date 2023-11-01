7329b50c2be7d14bfcc0c5cd6015f9a3e14643c5
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-11-01 16:47:32 +0900

    test

--- script.js
+++ script.js
@@ -791,6 +791,41 @@ async function create_afset_instance()
   return buff
 }
 
+
+function calculate_elmreaction_constdmg(reaction_coeff) 
+{
+  let reaction_count = 0;
+  if (char_propaty[0] == 0)
+  {
+    const Overloaded_count = parseInt(document.getElementById(Overloaded).value);
+    const Burgeon_count = parseInt(document.getElementById(Burgeon).value);
+    reaction_count = Overloaded_count * 2 + Burgeon_count * 3;
+  }
+  else if (char_propaty[0] == 1)
+  {
+    const Electro_Charged_count = parseInt(document.getElementById(Electro_Charged).value);
+    reaction_count = Electro_Charged_count * 1.2;
+  }
+  else if (char_propaty[0] == 3)
+  {
+    const Overloaded_count = parseInt(document.getElementById(Overloaded).value);
+    const Hyperbloom_count = parseInt(document.getElementById(Hyperbloom).value);
+    const Electro_Charged_count = parseInt(document.getElementById(Electro_Charged).value);
+    reaction_count = Overloaded_count * 2 + Hyperbloom_count * 3 + Electro_Charged_count * 1.2;
+  }
+  else if (char_propaty[0] == 4)
+  {
+    const Swirl_pyro_count = parseInt(document.getElementById(Swirl_pyro).value);
+    const Swirl_hydro_count = parseInt(document.getElementById(Swirl_hydro).value);
+    const Swirl_cyro_count = parseInt(document.getElementById(Swirl_cyro).value);
+    const Swirl_electro_count = parseInt(document.getElementById(Swirl_electro).value);
+    reaction_count = (Swirl_pyro_count + Swirl_hydro_count + Swirl_cyro_count + Swirl_electro_count) * 0.8;
+  }
+  reaction_count = reaction_count * 2 * 1446.85 * reaction_coeff;
+
+  return reaction_count;
+}
+
 ///////////////////////
 async function calculateEnemyProps(charDebuff, weaponDebuff) {
   const levelSelect = document.getElementById("char_level");
@@ -859,6 +894,7 @@ async function calculate_my_exp_dmg (base_status,af_main_status_buff,depend_stat
   const af_cd = parseFloat(document.getElementById("af_cd").value)/100;//聖遺物会心ダメージ上昇量
   const af_buff = [af_hp, af_deff, af_elm, af_elm_charge, af_attck, af_cr, af_cd];
   const char_parameter = await import_char_parameter();
+  const const_dmg =  calculate_elmreaction_constdmg(char_parameter[1]);
   let zetsuen_check = 0;
   if (selectedImageIds[0] ==17 && selectedImageIds[1] == 17 && attack_method_index == 4)
   {
@@ -968,7 +1004,7 @@ async function calculate_my_exp_dmg (base_status,af_main_status_buff,depend_stat
   basic_dmg = await char_instance.calculate_basic_dmg(dmg_rate);
   console.log(basic_dmg);
   exp_dmg = basic_dmg *(1 + result_status[5]*result_status[6])
-    *(1 + result_status[7]);
+    *(1 + result_status[7]) + const_dmg *  16 * result_status[2] / (result_status[2] + 2000);
   result_status.push(exp_dmg);
   return result_status;
 }
@@ -1013,6 +1049,7 @@ async function monte_carlo_calculate()
   const characterInfo = document.getElementById("characterInfo");
   const checkboxes = characterInfo.querySelectorAll('input[type="checkbox"]');
   const char_parameter = await import_char_parameter();
+  const const_dmg =  calculate_elmreaction_constdmg(char_parameter[1]);
 
   checkboxes.forEach((checkbox) => {
     checkboxStates.push(checkbox.checked);
@@ -1182,7 +1219,7 @@ async function monte_carlo_calculate()
 
       basic_dmg = await char_instance.calculate_basic_dmg(dmg_rate);
       exp_dmg = basic_dmg*(1 + result_status[5]*result_status[6])
-        *(1 + result_status[7]);
+        *(1 + result_status[7]) + const_dmg *  16 * result_status[2] / (result_status[2] + 2000);
 
       if (temp_exp_dmg < exp_dmg)
       {
@@ -1298,7 +1335,7 @@ async function monte_carlo_calculate()
 
       basic_dmg = await char_instance.calculate_basic_dmg(dmg_rate);
       exp_dmg = basic_dmg*(1 + result_status[5]*result_status[6])
-        *(1 + result_status[7]);
+        *(1 + result_status[7]) + const_dmg *  16 * result_status[2] / (result_status[2] + 2000);
       critical_dmg = basic_dmg*(1 + result_status[6])
         *(1 + result_status[7]);
       
