9c3ea04a85359a37d4e3bb31c0392506d4a3ade6
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-11-27 02:16:17 +0900

    test

--- show_char_statsform.js
+++ show_char_statsform.js
@@ -784,7 +784,6 @@ async function show_char_statsform()
         { text: "元素スキル（霜の襲撃）", value: "16" },
         { text: "元素爆発（凛冽なる輪舞）", value: "21" },
       ];
-    
       createchar_attackmethod(options);
     }
 
