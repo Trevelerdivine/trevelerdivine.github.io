57711a7dcfc0039ba327da1320c92594aeda453c
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-12-01 00:41:35 +0900

    test

--- script.js
+++ script.js
@@ -481,6 +481,9 @@ async function create_char_instance(base_status, parameter) {
     case "58":
       char_instance = new kirara(base_status, parameter);
       break;
+    case "62":
+      char_instance = new travelardendro(base_status, parameter);
+      break;
     case "63":
       char_instance = new aratakiitto(base_status, parameter);
       break;
