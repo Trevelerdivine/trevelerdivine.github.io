69403fd348ec7dc6d0472360a5f1968cc24c05b6
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-12-02 02:13:59 +0900

    test

--- data/character/char_class/char_class.js
+++ data/character/char_class/char_class.js
@@ -5943,8 +5943,8 @@ class alhaitham {
                           + parseInt(document.getElementById("alhaitham_skill_count4").value) * 3;
 
       const dmg_attck_rate1 = parseFloat(data["元素スキル"]["詳細"][0]["数値"][this.parameter[3]]) * attack_count1
-      const dmg_elm_rate1 = parseFloat(data["元素スキル"]["詳細"][1]["数値"][this.parameter[3]]) * attack_count1
-      const dmg_attck_rate2 = parseFloat(data["元素スキル"]["詳細"][2]["数値"][this.parameter[3]]) * attack_count2;
+      const dmg_elm_rate1 = parseFloat(data["元素スキル"]["詳細"][2]["数値"][this.parameter[3]]) * attack_count1
+      const dmg_attck_rate2 = parseFloat(data["元素スキル"]["詳細"][1]["数値"][this.parameter[3]]) * attack_count2;
       const dmg_elm_rate2 = parseFloat(data["元素スキル"]["詳細"][3]["数値"][this.parameter[3]]) * attack_count2;
       dmg_rate = [0, 0, [dmg_elm_rate1,dmg_elm_rate2], 0, [dmg_attck_rate1,dmg_attck_rate2], 0, 0];
     }
