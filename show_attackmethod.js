6c5ef790a67e6a09d81fa9ec614880636278dd92
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-12-05 01:47:02 +0900

    test

--- show_attackmethod.js
+++ show_attackmethod.js
@@ -543,25 +543,38 @@ async function show_attack_method()
   }
   else if (selectedCharId == "74")
   {
+    if (char_constellations == 0)
+    {
+      const furina_tention_text = createTextNode("　テンション：");
+      const furina_tention_list = createSelectList("furina_tention1", 0, 300, "", "ポイント", 300);
+      attack_method_prop.appendChild(furina_tention_text);
+      attack_method_prop.appendChild(furina_tention_list);
+      attack_method_prop.appendChild(document.createElement("br"));
+    }
+    else if (char_constellations == 1)
+    {
+      const furina_tention_text = createTextNode("　テンション：");
+      const furina_tention_list = createSelectList("furina_tention1", 0, 400, "", "ポイント", 400);
+      attack_method_prop.appendChild(furina_tention_text);
+      attack_method_prop.appendChild(furina_tention_list);
+      attack_method_prop.appendChild(document.createElement("br"));
+    }
+    else
+    {
+      const furina_tention_text = createTextNode("　テンション：");
+      const furina_tention_list = createSelectList("furina_tention1", 0, 400, "", "ポイント", 400);
+      attack_method_prop.appendChild(furina_tention_text);
+      attack_method_prop.appendChild(furina_tention_list);
+      attack_method_prop.appendChild(document.createElement("br"));
+      const furina_tention_text2 = createTextNode("　超過テンション：");
+      const furina_tention_list2 = createSelectList("furina_tention2", 0, 400, "", "ポイント", 400);
+      attack_method_prop.appendChild(furina_tention_text2);
+      attack_method_prop.appendChild(furina_tention_list2);
+      attack_method_prop.appendChild(document.createElement("br"));
+    }
+
     if (attack_method == 1)
     {
-      if (char_constellations < 1)
-      {
-        const furina_tention_text = createTextNode("　テンション：");
-        const furina_tention_list = createSelectList("furina_tention1", 0, 300, "", "ポイント", 300);
-        attack_method_prop.appendChild(furina_tention_text);
-        attack_method_prop.appendChild(furina_tention_list);
-        attack_method_prop.appendChild(document.createElement("br"));
-      }
-      else
-      {
-        const furina_tention_text = createTextNode("　テンション：");
-        const furina_tention_list = createSelectList("furina_tention1", 0, 300, "", "ポイント", 300);
-        attack_method_prop.appendChild(furina_tention_text);
-        attack_method_prop.appendChild(furina_tention_list);
-        attack_method_prop.appendChild(document.createElement("br"));
-      }
-
       options = [
         { text: "１段目", value: "0", checked: true },
         { text: "２段目", value: "1" },
