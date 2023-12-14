bc379e25b25b28ec17652f93cd8e69a8baca62be
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-12-14 17:14:37 +0900

    fix

--- script.js
+++ script.js
@@ -949,10 +949,10 @@ async function create_weapon_instance(base_status) {
       weapon_instance = new HakushinRing(base_status);
       break;
     case "140":
-      weapon_instance = new WineandSong(base_status);
+      weapon_instance = new DodocoTales(base_status);
       break;
     case "141":
-      weapon_instance = new DodocoTales(base_status);
+      weapon_instance = new WineandSong(base_status);
       break;
     case "142":
       weapon_instance = new Frostbearer(base_status);
