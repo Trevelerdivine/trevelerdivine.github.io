b86fed51ddec565c45473eb20db587fab28dad88
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-12-21 01:21:05 +0900

    fix bug

--- script.js
+++ script.js
@@ -1330,7 +1330,7 @@ function create_reactionbonus_list(){
       reaction_bonus_list[7] = 0.4;
     }
   }
-  else if (char_propaty[5] == 3)
+  else if (char_propaty[0] == 5)
   {
     const Nirou_HP = parseInt(document.getElementById("Nirou_HP").value);
     reaction_bonus_list[4] = Math.min(Math.max(Nirou_HP - 30000, 0) * 0.00009, 4);
