da684086a8cec11ada10597f09f86184f7c93a9d
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2023-12-21 00:57:42 +0900

    test

--- script.js
+++ script.js
@@ -1314,6 +1314,8 @@ function create_reactionbonus_list(){
     {
       reaction_bonus_list[2] = 0.4;
     }
+    const Nirou_HP = parseInt(document.getElementById("Nirou_HP").value);
+    reaction_bonus_list[4] = Math.min(Math.min(Nirou_HP - 30000, 0) * 0.00009, 4);
   }
   else if (char_propaty[0] == 3)
   {
