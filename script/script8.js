9668e1a043f3f5ab0a234ddef87a4153436a5fcd
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-09-11 22:30:26 +0900

    fix

--- script/script8.js
+++ script/script8.js
@@ -2130,11 +2130,11 @@ async function monte_carlo_calculate()
     document.getElementById("goblet3").innerHTML = (AfPartsNum[2] !== 0 ? (AfPartsRate[2] * 100 / AfPartsNum[2]).toFixed(1) : "100") + "％";
     document.getElementById("goblet4").innerHTML = (AfPartsNum[3] !== 0 ? (AfPartsRate[3] * 100 / AfPartsNum[3]).toFixed(1) : "100") + "％";
     document.getElementById("goblet5").innerHTML = (AfPartsNum[4] !== 0 ? (AfPartsRate[4] * 100 / AfPartsNum[4]).toFixed(1) : "100") + "％";
-    document.getElementById("circlet1").innerHTML = (1 - (1 -AfPartsNum[0] / TryCount) ** 12).toFixed(1) + "％";
-    document.getElementById("circlet2").innerHTML = (1 - (1 -AfPartsNum[1] / TryCount) ** 12).toFixed(1) + "％";
-    document.getElementById("circlet3").innerHTML = (1 - (1 -AfPartsNum[2] / TryCount) ** 6).toFixed(1) + "％";
-    document.getElementById("circlet4").innerHTML = (1 - (1 -AfPartsNum[3] / TryCount) ** 3).toFixed(1) + "％";
-    document.getElementById("circlet5").innerHTML = (1 - (1 -AfPartsNum[4] / TryCount) ** 4).toFixed(1) + "％";
+    document.getElementById("circlet1").innerHTML = ((1 - (1 -AfPartsNum[0] / TryCount) ** 12) * 100).toFixed(1) + "％";
+    document.getElementById("circlet2").innerHTML = ((1 - (1 -AfPartsNum[1] / TryCount) ** 12) * 100).toFixed(1) + "％";
+    document.getElementById("circlet3").innerHTML = ((1 - (1 -AfPartsNum[2] / TryCount) ** 6) * 100).toFixed(1) + "％";
+    document.getElementById("circlet4").innerHTML = ((1 - (1 -AfPartsNum[3] / TryCount) ** 3) * 100).toFixed(1) + "％";
+    document.getElementById("circlet5").innerHTML = ((1 - (1 -AfPartsNum[4] / TryCount) ** 4) * 100).toFixed(1) + "％";
 
     
     console.timeEnd('myTimer'); // タイマーを終了し、経過時間をコンソールに表示
