39c3fbde92544ba3c7461d797894e631497e0e94
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-11-02 17:52:06 +0900

    test

--- script/script7.js
+++ script/script7.js
@@ -1959,7 +1959,7 @@ async function monte_carlo_calculate()
     const depend_status = await calculate_depend_status();
     const team_fix_buff = await calculate_team_fix_buff(base_status);
     const team_dynamic_buff = await calculate_team_dynamic_buff(base_status);
-    const TryCount = 1000000;
+    const TryCount = 2000000;
     let my_result_status = await calculate_my_exp_dmg(base_status,af_main_status_buff,depend_status);
     let my_exp_dmg = my_result_status[8];
     let response = "";
